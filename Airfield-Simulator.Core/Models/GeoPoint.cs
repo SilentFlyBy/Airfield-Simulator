0fde0eeedba44ee722d5d5d9755020a2fca20139
Erik Moldtmann <erik@moldtmann.de>
2017-01-03 19:50:24 +0100

    add heading calculation between two points

--- Airfield-Simulator.Core/Models/GeoPoint.cs
+++ Airfield-Simulator.Core/Models/GeoPoint.cs
@@ -18,9 +18,45 @@ namespace Airfield_Simulator.Core.Models
             Y = y;
         }
 
+        public double GetDistanceTo(GeoPoint point)
+        {
+            return GetDistance(this, point);
+        }
+
+        public double GetHeadingTo(GeoPoint point)
+        {
+            return GetHeading(this, point);
+        }
+
         public static double GetDistance(GeoPoint point_a, GeoPoint point_b)
         {
             return Math.Round(Math.Sqrt(Math.Pow((point_b.X - point_a.X), 2)+ Math.Pow((point_b.Y - point_a.Y), 2)), 1);
+        }
+
+        public static double GetHeading(GeoPoint point_a, GeoPoint point_b)
+        {
+            double a = point_a.X - point_b.X;
+            double b = point_a.Y - point_b.Y;
+
+            double c = Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
+            double q = Math.Pow(a, 2) / c;
+            double p = c - q;
+            double h = Math.Sqrt(p * q);
+            double alpha = Math.Atan(h / p);
+
+            double degrees = alpha * (360 / (2 * Math.PI));
+
+            if (point_a.X < point_b.X && point_a.Y < point_b.Y)
+                return degrees;
+
+            else if (point_a.X > point_b.X && point_a.Y < point_b.Y)
+                return 360 - degrees;
+
+            else if (point_a.X > point_b.X && point_a.Y > point_b.Y)
+                return 180 + degrees;
+
+            else
+                return 180 - degrees;
         }
     }
 }
--- Airfield-Simulator.CoreTests/Models/GeoPointTests.cs
+++ Airfield-Simulator.CoreTests/Models/GeoPointTests.cs
@@ -20,5 +20,84 @@ namespace Airfield_Simulator.Core.Models.Tests
             double distance = GeoPoint.GetDistance(point1, point2);
             Assert.That(distance, Is.EqualTo(24.5));
         }
+
+        [Test()]
+        public void GetDistanceToTest()
+        {
+            GeoPoint point1 = new GeoPoint(2, 2);
+            GeoPoint point2 = new GeoPoint(4, 5);
+
+            double distance = GeoPoint.GetDistance(point1, point2);
+            double distanceto = point1.GetDistanceTo(point2);
+
+            Assert.That(distance, Is.EqualTo(distanceto));
+        }
+
+        [Test()]
+        public void GetHeadingToTest()
+        {
+            GeoPoint point1 = new GeoPoint(2, 2);
+            GeoPoint point2 = new GeoPoint(4, 5);
+
+            double heading = GeoPoint.GetHeading(point1, point2);
+            double headingto = point1.GetHeadingTo(point2);
+
+            Assert.That(heading, Is.EqualTo(headingto));
+        }
+
+        [Test()]
+        public void GetHeadingTest()
+        {
+            //Quadrant 1
+            GeoPoint point1 = new GeoPoint(2, 2);
+            GeoPoint point2 = new GeoPoint(4, 5);
+            double heading = GeoPoint.GetHeading(point1, point2);
+
+            Assert.That(heading, Is.LessThan(45));
+
+            point2.Y = 4;
+            point2.X = 5;
+            heading = GeoPoint.GetHeading(point1, point2);
+
+            Assert.That(heading, Is.GreaterThan(45));
+
+            //Quadrant 2
+            point2.X = 0;
+            point2.Y = 5;
+            heading = GeoPoint.GetHeading(point1, point2);
+
+            Assert.That(heading, Is.GreaterThan(315));
+
+            point2.Y = 4;
+            point2.X = -1;
+            heading = GeoPoint.GetHeading(point1, point2);
+
+            Assert.That(heading, Is.LessThan(315));
+
+            //Quadrant 3
+            point2.Y = 0;
+            heading = GeoPoint.GetHeading(point1, point2);
+
+            Assert.That(heading, Is.GreaterThan(225));
+
+            point2.X = 0;
+            point2.Y = -1;
+
+            heading = GeoPoint.GetHeading(point1, point2);
+
+            Assert.That(heading, Is.LessThan(225));
+
+            //Quadrant 4
+            point2.X = 4;
+            heading = GeoPoint.GetHeading(point1, point2);
+
+            Assert.That(heading, Is.GreaterThan(135));
+
+            point2.X = 5;
+            point2.Y = 0;
+            heading = GeoPoint.GetHeading(point1, point2);
+
+            Assert.That(heading, Is.LessThan(135));
+        }
     }
 }
